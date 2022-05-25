5c24efedca0f6d7b4706186acf7942ab6e52bb57
Blaž Čulina <blaz.culina1911@gmail.com>
2022-05-25 23:59:32 +0200

    Phonebook CRUD, exercises: 2.15-2.18 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,72 +1,76 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
+import personService from "./services/personsService"
 import InputForm from "./components/InputForm"
 import Phonebook from "./components/Phonebook";
 import SearchFilter from "./components/SearchFilter";
+
 function App() {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState("")
-  const [newPhone, setNewPhone] = useState("")
-  const [filter, setFilter] = useState("")
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState("")
+    const [newNumber, setNewNumber] = useState("")
+    const [filter, setFilter] = useState("")
 
-  const handleNameChange = (event) => setNewName(event.target.value);
-  const handlePhoneChange = (event) => setNewPhone(event.target.value);
-  const handleFilterChange = (event) => setFilter(event.target.value)
+    const handleNameChange = (event) => setNewName(event.target.value);
+    const handlePhoneChange = (event) => setNewNumber(event.target.value);
+    const handleFilterChange = (event) => setFilter(event.target.value)
 
-  const hook = () => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then(promise => setPersons(promise.data))
-  }
-
-  useEffect(hook, [])
-
-  const addNewPerson = (event) => {
-    event.preventDefault();
-
-    const personIds = persons.map((p) => p.id);
-    const personObject = {
-      name: newName,
-      phone: newPhone,
-    };
-    //Converting objects to JSON format for object comparison
-    //There must exist a better way! :(
-    const stringifiedObject = JSON.stringify(personObject);
-    const stringifiedPersons = persons.map((p) => JSON.stringify(
-      { //Removed id from comparison
-        name: p.name,
-        phone: p.phone
-      }))
-
-    if (stringifiedPersons.includes(stringifiedObject)) {
-      alert(`${personObject.name} is already added to phonebook`)
-      return
+    const hook = () => {
+        personService.getAll()
+            .then(request => setPersons(request))
     }
-      personObject.id = personIds.length
-        ? personIds[personIds.length - 1] + 1
-        : 0;
-      setPersons(persons.concat(personObject))
-  }
 
-  const filteredPersons = filter ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase())) : persons
+    useEffect(hook, [])
 
-  return (
-    <div>
+    const addNewPerson = (event) => {
+        event.preventDefault();
 
-      <h1>Phonebook</h1>
+        const newPersonObject = {name: newName, number: newNumber}
+        const personNames = persons.map(p => p.name)
 
-      <h2>add a new person</h2>
+        console.log(personNames)
 
-      <InputForm addNewPerson={addNewPerson} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange}/>
+        if (personNames.includes(newName)) {
+            const id = persons.find(p => p.name === newName).id
+            personService.update(newPersonObject, id)
+                .then(data => setPersons(
+                    persons.slice(0, persons.length - 1)
+                        .concat(data)
+                ))
+            return
+        }
 
-      <h2>Contacts</h2>
+        personService.create(newPersonObject)
+            .then(data => setPersons(persons.concat(data)))
+    }
 
-      <SearchFilter func={handleFilterChange}/>
-      <br />
-      <Phonebook filteredPersons={filteredPersons}/>
+    const deleteFunction = (id) => {
+        personService
+            .remove(id)
+            .then(() => {
+                personService.getAll()
+                    .then(request => setPersons(request))
+            })
+    }
 
-    </div>
-  );
+    const filteredPersons = filter ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase())) : persons
+
+    return (<div>
+
+        <h1>Phonebook</h1>
+
+        <h2>add a new person</h2>
+
+        <InputForm addNewPerson={addNewPerson} handleNameChange={handleNameChange}
+                   handlePhoneChange={handlePhoneChange}/>
+
+        <h2>Contacts</h2>
+
+        <SearchFilter func={handleFilterChange}/>
+        <br/>
+        <Phonebook filteredPersons={filteredPersons} deleteFunction={deleteFunction}/>
+
+    </div>);
 }
 
 export default App;
--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,10 +1,13 @@
-const Phonebook = ({filteredPersons}) => {
+const Phonebook = ({filteredPersons, deleteFunction}) => {
     return (
         <div>
             {filteredPersons.map((person) => {
                 return <li key={person.id}>{person.name}
                     <ul>
                         <li>{person.number}</li>
+                    </ul>
+                    <ul>
+                        <button onClick={() => deleteFunction(person.id)}>delete</button>
                     </ul>
                 </li>;
             })}
