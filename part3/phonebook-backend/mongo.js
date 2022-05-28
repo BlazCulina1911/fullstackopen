e62c4ae60b0ac991b9ab9d5c77ce6bdeaf3df192
Blaž Čulina <blaz.culina1911@gmail.com>
2022-05-28 23:49:15 +0200

    Added mongo.js file and completed exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,52 @@
+const mongo = require('mongo');
+const { default: mongoose } = require('mongoose');
+
+let mode = 'noAction'
+let name = undefined
+let number = undefined
+
+if (process.argv.length < 3){
+    console.log("Please provide a password!")
+    process.exit(1)
+}
+
+if (process.argv.length < 4){
+    mode = 'find'
+    console.log("Attempting to list all people in PhoneBookApp ")
+} else {
+    mode = 'save'
+    name = process.argv[3]
+    number = process.argv[4]
+    console.log(`Attempt to save ${name} with number ${number} to the personsApp`)
+}
+const password = process.argv[2];
+
+const url = `mongodb+srv://blaz:${password}@phonebookcluster.zjcve.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(mode === 'find'){
+    Person.find().then(result => {
+        result.forEach(x =>
+            console.log(`${x.name} ${x.number}`)
+        )
+        mongoose.connection.close()
+    })
+} else if (mode === 'save'){
+    const person = new Person({
+        name,
+        number
+    })
+
+    person.save().then(() => {
+        console.log("SUCCESS")
+        mongoose.connection.close()
+    })
+}
