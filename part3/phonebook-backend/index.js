980d41c359b1e304cce5e77b212aaab4b24980e3
Blaž Čulina <blaz.culina1911@gmail.com>
2022-05-28 15:48:17 +0200

    heroku init

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,10 @@
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.static('build'))
 const morgan = require('morgan')
 
 
